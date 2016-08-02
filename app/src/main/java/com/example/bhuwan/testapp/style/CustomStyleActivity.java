2572444179a222c6563c370b5ee7126a8945290f
bhuwang <bhuwangautam@lftechnology.com>
2016-08-02 16:05:19 +0545

    completed styles and inheritance

---
+++ app/src/main/java/com/example/bhuwan/testapp/style/CustomStyleActivity.java
@@ -0,0 +1,15 @@
+package com.example.bhuwan.testapp.style;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bhuwan.testapp.R;
+
+public class CustomStyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_style);
+    }
+}
